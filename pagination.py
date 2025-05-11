5d1fa5089538a9660ad4c22a7d83f8d54155e283
dmswjd37 <115202586+dmswjd37@users.noreply.github.com>
2025-05-11 23:32:07 +0900

    #6.5 Pagination

--- pagination.py
+++ pagination.py
@@ -4,16 +4,17 @@ from bs4 import BeautifulSoup
 scraper = cloudscraper.create_scraper() # returns a requests.Session object
 
 all_jobs = []
-url = "https://weworkremotely.com/remote-full-time-jobs?page=1"
+
 def scrape_page(url):
     print(f"Scrapping {url}...")
     response = scraper.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+    jobs = soup.find("section", class_="jobs").find_all("li")[0:-1]
 
     for job in jobs:
         title = job.find("h4", class_="new-listing__header__title").text
-        region = job.find("p", class_="new-listing__company-headquarters").text
+        region_tag = job.find("p", class_="new-listing__company-headquarters")
+        region = region_tag.get_text if region_tag else "N/A"
         company = job.find("p", class_="new-listing__company-name").text
         url = job.find("div", class_="tooltip--flag-logo").next_sibling["href"]
         
@@ -25,12 +26,16 @@ def scrape_page(url):
         }
         all_jobs.append(job_data)
 
-# response = scraper.get("https://weworkremotely.com/remote-full-time-jobs?page=1")
-# soup = BeautifulSoup(response.text, "html.parser")
-# buttons = len(soup.find("div", class_="pagination").find_all("span", class_="page"))
+def get_pages(url):
+    response = scraper.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-# for x in range(buttons):
-#     url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
-#     scrape_page(url)
-scrape_page(url)
+    return len(soup.find("div", class_="pagination").find_all("span", class_="page"))
+
+total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
+
+for x in range(total_pages):
+    url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
+    scrape_page(url)
+
 print(len(all_jobs))
