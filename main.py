372c1a6ab72c381350dbe32c3efb8a6f6f4e312f
dmswjd37 <dmswjddid37@naver.com>
2025-05-29 18:26:25 +0900

    Form 작성

--- main.py
+++ main.py
@@ -1,31 +1,13 @@
-import cloudscraper
-from bs4 import BeautifulSoup
+from flask import Flask, render_template
 
-scraper = cloudscraper.create_scraper() # returns a requests.Session object
+app = Flask("JobScrapper")
 
-all_jobs = []
+@app.route("/")
+def home():
+    return render_template("home.html", name="nico")
 
-def scrape_page(url):
-    response = scraper.get(url)
+@app.route("/search")
+def hello():
+    return 'hello you!'
 
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    jobs = soup.find("section", id="category-2").find_all("li")[1:-1]
-
-    for job in jobs:
-        title = job.find("h4", class_="new-listing__header__title").text
-        region = job.find("p", class_="new-listing__company-headquarters").text
-        company = job.find("p", class_="new-listing__company-name").text
-        url = job.find("div", class_="tooltip--flag-logo").next_sibling["href"]
-        
-        job_data = {
-            "title": title,
-            "region": region,
-            "company": company,
-            "url": f"https://weworkremotely.com/{url}"
-        }
-        all_jobs.append(job_data)
-
-url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
-
-print(all_jobs)
+app.run()
