2d24925b0a9df22b8b36951e3ba70f29d394049f
dmswjd37 <dmswjddid37@naver.com>
2025-05-30 18:26:03 +0900

    For Loops 작성 중

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
 
 app = Flask("JobScrapper")
 
@@ -7,7 +9,11 @@ def home():
     return render_template("home.html", name="nico")
 
 @app.route("/search")
-def hello():
-    return 'hello you!'
+def search():
+    keyword = request.args.get("keyword")
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = indeed + wwr
+    return render_template("search.html", keyword=keyword, jobs=jobs)
 
-app.run()
+app.run("127.0.0.1", port=8000, debug=True)
